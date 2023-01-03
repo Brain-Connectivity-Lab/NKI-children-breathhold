df71ffd839f6374946da0e878aea685f5a76e9ea
chendonna3 <chendonna3@gmail.com>
2023-01-03 12:40:54 -0500

    Add dartel template code

---
+++ codes/preproc_06_dartel_template.m
@@ -0,0 +1,75 @@
+% Preprocessing for NKI-children-breathhold data
+% Create Dartel Template
+
+clc; clear
+path = '/mnt/disk1/NKI_Development_Data/longitudinal/child/all-visits/'; % Specify where subjects' data are
+
+% Load subject names to preprocess
+load('../data/finalAllSubjID.mat')
+cd(path)
+
+% Specify session ('BAS1','FLU1', or 'FLU2')
+session = 'BAS1';
+
+switch session
+    case 'BAS1'
+        subj_list = subB1T1BHa18HC;
+    case 'FLU1'
+        subj_list = subF1T1BHa18HC;
+    case 'FLU2'
+        subj_list = subF2T1BHa18HC;
+end
+
+
+%% Create Dartel Template
+
+% Anatomical Dartel Imports - Grey Matter & White Matter & CSF
+dartel_gm = cell(length(subj_list),1);
+dartel_wm = cell(length(subj_list),1);
+dartel_csf = cell(length(subj_list),1);
+for subji = 1:length(subj_list)
+    % Define anatomical folder
+    anat_folder = [path 'sub-' subj_list{subji} '/ses-' session '/anat/'];
+    
+    % Dartel imports
+    dartel_gm{subji,1} = ([anat_folder 'prepro_breathhold/rc1sub-' subj_list{subji} '_ses-' session '_T1w.nii,1']);
+    dartel_wm{subji,1} = ([anat_folder 'prepro_breathhold/rc2sub-' subj_list{subji} '_ses-' session '_T1w.nii,1']);
+    dartel_csf{subji,1} = ([anat_folder 'prepro_breathhold/rc3sub-' subj_list{subji} '_ses-' session '_T1w.nii,1']);
+end
+
+matlabbatch{1}.spm.tools.dartel.warp.images = {
+    dartel_gm
+    dartel_wm
+    dartel_csf
+    }';
+matlabbatch{1}.spm.tools.dartel.warp.settings.template = 'Template';
+matlabbatch{1}.spm.tools.dartel.warp.settings.rform = 0;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).rparam = [4 2 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(1).slam = 16;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).rparam = [2 1 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(2).slam = 8;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).rparam = [1 0.5 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).K = 1;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(3).slam = 4;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).rparam = [0.5 0.25 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).K = 2;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(4).slam = 2;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).K = 4;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(5).slam = 1;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).K = 6;
+matlabbatch{1}.spm.tools.dartel.warp.settings.param(6).slam = 0.5;
+matlabbatch{1}.spm.tools.dartel.warp.settings.optim.lmreg = 0.01;
+matlabbatch{1}.spm.tools.dartel.warp.settings.optim.cyc = 3;
+matlabbatch{1}.spm.tools.dartel.warp.settings.optim.its = 3;
+
+spm_jobman('run',matlabbatch)
